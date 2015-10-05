ec326ed97243d88ead6a2f446b31d967ceb5c087
Pedro <pedro@pedro-K55VM.(none)>
2015-10-05 21:53:01 +0100

    Adicionadas proteccoes nos arguementos de entrada das funcoes

--- source/table.c
+++ source/table.c
@@ -14,55 +14,71 @@ int hashcode(struct table_t *table, char *key){
 	{
 		int i;
 		for (i = 0; i < strlen(key); ++i)
-		{
 			val += 0 + key[i];
-		}
 	}
 	//se tem mais que 6 letras
 	else
 	{
+		int i;
+		for (i = 0; i < 3; i++)
+			val += key[i];
+	/*
 		val += key[0];
 		val += key[1];
 		val += key[2];
+	*/
+
+		for (i = 3; i > 0; i--)
+			val += key[strlen(key) - i];
+
+	/*
 		val += key[strlen(key) - 3];
 		val += key[strlen(key) - 2];
 		val += key[strlen(key) - 1];
+	*/
 	}
 
 	return (val % table->num_places);
 
 }
 
+
 struct table_t *table_create(int n) {
-	
+	if (n < 0)
+		return NULL;
+
 	struct table_t *t = ( struct table_t * ) malloc(sizeof( struct table_t ));
 	t->num_places = n;
 	t->places = (struct list_t **) malloc( sizeof( struct list_t *) * n );
+
 	int i;
 	for (i = 0; i < n; ++i)
-	{
 		t->places[i] = NULL;
-	}
+
 	return t;
 }
 
 
 void table_destroy(struct table_t *table){
+	if (table == NULL)
+		return;
 
 	int size = table->num_places;
 	int i;
+
 	for (i = 0; i < size; ++i)
 	{
 		//se o place ja tem lista
 		if ( table->places[i] != NULL )
-		{
 			list_destroy(table->places[i]);
-		}	
 	}
 	free(table);
 }
 
+
 int table_put(struct table_t *table, char *key, struct data_t *value){
+	if (table == NULL || key == NULL || data == NULL)
+		return -1;
 
 	struct entry_t *entry = entry_create(key, value);
 	int pos = hashcode(table, key);
@@ -81,20 +97,17 @@ int table_put(struct table_t *table, char *key, struct data_t *value){
 	}
 
 	if( res == 0 )
-	{
 		table->size = table->size + 1;
-	}
-
 
 	return res;
 }
 
-int table_update(struct table_t *table, char *key, struct data_t *value){
 
+int table_update(struct table_t *table, char *key, struct data_t *value){
 	if ( table == NULL || key == NULL || value == NULL )
 		return -1;
 
-		//obtem hashcode
+	//obtem hashcode
 	int pos = hashcode(table, key);
 
 	struct entry_t *entry = list_get( table->places[pos] ,key);
@@ -108,8 +121,8 @@ int table_update(struct table_t *table, char *key, struct data_t *value){
 	return 0;
 }
 
-struct data_t *table_get(struct table_t *table, char *key){
 
+struct data_t *table_get(struct table_t *table, char *key){
 	if ( table == NULL || key == NULL )
 		return NULL;
 
@@ -122,8 +135,8 @@ struct data_t *table_get(struct table_t *table, char *key){
 
 }
 
-int table_del(struct table_t *table, char *key){
 
+int table_del(struct table_t *table, char *key){
 	if ( table == NULL || key == NULL )
 		return -1;
 
@@ -150,22 +163,25 @@ int table_del(struct table_t *table, char *key){
 
 }
 
-int table_size(struct table_t *table){
 
+int table_size(struct table_t *table){
 	if ( table == NULL )
-		return 0;
+		return -1;
 
 	return table->size;
 }
 
-//faltam estas duas
+
+//TODO
 char **table_get_keys(struct table_t *table){
-	
+	if (table == NULL)
+		return NULL;
+
 	int i = 0;
 	int index = 0;
 	char **keys = (char **) malloc( (sizeof(char *) * table->size) + 1 );
 	for(i=0; i<table->size; i++){
-		
+
 		if( table->places[i] != NULL ){
 			char **l = list_get_keys(table->places[i]);
 			int j;
@@ -173,19 +189,22 @@ char **table_get_keys(struct table_t *table){
 				keys[index] = strdup(l[j]);
 				index++;
 			}
+
 			list_free_keys(l);
 		}
 	}
 	return keys;
 }
 
+
 void table_free_keys(char **keys){
-	
+	if (keys == NULL)
+		return;
+
 	int i;
 	for(i=0; keys[i]!=NULL; i++)
-	{
 		free(keys[i]);
-	}
+
 	free(keys);
 
 }
